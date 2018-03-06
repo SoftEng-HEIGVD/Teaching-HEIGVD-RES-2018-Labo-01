d9de38500a697cfe44017b267e7beab39e9b05ac
Adam <adam.zouari@heig-vd.ch>
2018-03-06 18:41:26 +0100

    UpperCaseFilterWriterTest success

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -2,6 +2,7 @@ package ch.heigvd.res.lab01.impl.filters;
 
 import java.io.FilterWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.io.Writer;
 
 /**
@@ -9,24 +10,37 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str) throws IOException {
+        String str2 = str.toUpperCase();
+        out.append(str2);
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        String str2 = str.substring(off, off + len);
+        write(str2);
+    }
 
+    @Override
+    public void write(char[] cbuf) throws IOException {
+        for(char c : cbuf)
+            write(c);
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        for(int i = off; i < off + len; i++)
+            write(cbuf[i]);
+    }   
+
+    @Override
+    public void write(int c) throws IOException {
+        out.append(Character.toUpperCase((char)c));
+    }
 }
