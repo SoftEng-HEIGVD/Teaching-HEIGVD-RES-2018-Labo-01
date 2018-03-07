b2c9b7847caa20984710f62b94987042717e0e3d
antonio.cusanelli <antonio.cusanelli@heig-vd.ch>
2018-03-07 09:34:23 +0100

    UpperCaseFilterWriter and FileNumberingFileWriter fixed

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -17,25 +17,45 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+	private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+	private int line = 1;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+	public FileNumberingFilterWriter(Writer out) {
+		super(out);
+	}
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	@Override
+	public void write(String str, int off, int len) throws IOException {
+		write(str.toCharArray(), off, len);
+	}
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	@Override
+	public void write(char[] cbuf, int off, int len) throws IOException {
+		/*
+			if first line we add the first line's number and tab and then visit char by
+			char to find the specific character of end of line in different OS
+		 */
+		if (line == 1)
+			out.write(line++ + "\t");
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+		for (int i = off; i < off + len; i++) {
+			out.write(cbuf[i]);
+			//condition : only \n or \r or the two together \n\r
+			if (cbuf[i] == '\n' || (cbuf[i] == '\r' && i < cbuf.length - 1 && cbuf[i + 1] != '\n' )
+								|| (cbuf[i] == '\r' && i == cbuf.length - 1))
+				out.write(line++ + "\t");
+		}
+	}
 
+
+	@Override
+	public void write(int c) throws IOException {
+		if (line == 1)
+			out.write(line++ + "\t");
+
+		out.write(c);
+
+		if (c == '\n')
+			out.write(line++ + "\t");
+	}
 }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -9,24 +9,28 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
+
   public UpperCaseFilterWriter(Writer wrappedWriter) {
     super(wrappedWriter);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  	//call the funct with char Array, we don't do the "same" code twice
+    write(str.toCharArray(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  	for(int i = off; i < off + len; i++){
+  		cbuf[i] = Character.toUpperCase(cbuf[i]);
+	}
+	out.write(cbuf, off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  	out.write(Character.toUpperCase(c));
   }
 
 }
