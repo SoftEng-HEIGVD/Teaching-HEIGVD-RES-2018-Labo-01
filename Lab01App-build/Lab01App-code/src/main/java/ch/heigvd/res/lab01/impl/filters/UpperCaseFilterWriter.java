7b886f299011247e6021331bb4ffc543f9c61123
admin <lagha.1993@gmail.com>
2018-03-07 09:41:28 +0100

    upperCaseFilterPassed

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -9,24 +9,43 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str) throws IOException {
+        out.write(str.toUpperCase());
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        str.substring(off, len);
+        out.write(str);
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        String str = "";
+        for (int i = 0; i < cbuf.length; ++i) {
+            str = str + cbuf[i];
+        }
+        write(str, off, len);
+    }
+
+    public void write(char[] cbuf) throws IOException {
+        String str = "";
+        for (int i = 0; i < cbuf.length; ++i) {
+            str = str + cbuf[i];
+        }
+        write(str);
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        String str = "" + (char) c;
+        out.write(str);
+    }
 
 }
