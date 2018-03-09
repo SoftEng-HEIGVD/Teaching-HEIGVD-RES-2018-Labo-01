04623e555610d21b9bdc84eebb28a08a8affda03
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-03-09 10:11:25 +0100

    utils + App + UpperCaseFilter

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -3,24 +3,39 @@ package ch.heigvd.res.lab01.impl;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Olivier Liechti
  */
 public class Utils {
 
-  private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+    private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
-  /**
-   * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
-   * @param lines a string that may contain 0, 1 or more lines
-   * @return an array with 2 elements; the first element is the next line with
-   * the line separator, the second element is the remaining text. If the argument does not
-   * contain any line separator, then the first element is an empty string.
-   */
-  public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    /**
+     * This method looks for the next new line separators (\r, \n, \r\n) to extract
+     * the next line in the string passed in arguments.
+     *
+     * @param lines a string that may contain 0, 1 or more lines
+     * @return an array with 2 elements; the first element is the next line with
+     * the line separator, the second element is the remaining text. If the argument does not
+     * contain any line separator, then the first element is an empty string.
+     */
+    public static String[] getNextLine(String lines) {
+        String[] result = new String[2];
+        result[0] = "";
+        result[1] = lines;
+
+        for (int i = 0; i < lines.length(); ++i) {
+            if (lines.charAt(i) == '\r' || lines.charAt(i) == '\n') {
+                if (i+1 < lines.length() && lines.charAt(i + 1) == '\n') {
+                    result[0] = lines.substring(0, i + 2);
+                    result[1] = lines.substring(i + 2, lines.length());
+                } else {
+                    result[0] = lines.substring(0, i + 1);
+                    result[1] = lines.substring(i + 1, lines.length());
+                }
+                break;
+            }
+        }
+        return result;
+    }
 
 }
