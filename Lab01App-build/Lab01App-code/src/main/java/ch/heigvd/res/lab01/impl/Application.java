cc53c14e3ef10b9a3e18155454f2ff152d6c7118
Guillaume.Blanco <guillaume.blanco@heig-vd.ch>
2018-03-09 17:59:08 +0100

    application terminer mais pas tous les tests en vert

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Application.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Application.java
@@ -129,12 +129,17 @@ public class Application implements IApplication {
   void storeQuote(Quote quote, String filename) throws IOException {
       List<String> listDirPath = quote.getTags();
       if(!listDirPath.isEmpty()){
-          StringBuilder dirPath = new StringBuilder(listDirPath.get(0));
+          StringBuilder dirPath = new StringBuilder(WORKSPACE_DIRECTORY);
+         // StringBuilder dirPath = new StringBuilder("/quotes");
           for(String dir : listDirPath) {
               dirPath.append("/");
               dirPath.append(dir);
           }
-          System.out.println(dirPath);
+          //System.out.println(dirPath);
+          File  directories = new File(dirPath.toString());
+          directories.mkdirs();
+          File fileQuote = new File(dirPath + "/" + filename);
+          fileQuote.createNewFile();
       }
   }
 
@@ -152,6 +157,21 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+
+              File[] list = file.listFiles();
+
+              if (list == null) return;
+
+              for ( File f : list ) {
+                  if ( f.isDirectory() ) {
+                      System.out.println( "Dir:" + f.getAbsoluteFile() );
+                      visit(f);
+                  }
+                  else {
+                      System.out.println( "File:" + f.getAbsoluteFile() );
+                  }
+              }
+
       }
     });
   }
