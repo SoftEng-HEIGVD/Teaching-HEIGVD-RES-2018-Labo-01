223931b0e4cf37be4b382228e050f2547959e72d
Perceval <dnamella1@hotmail.com>
2018-03-10 12:06:22 +0100

    DFS implemented

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -2,6 +2,7 @@ package ch.heigvd.res.lab01.impl.explorers;
 
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
+
 import java.io.File;
 
 /**
@@ -9,14 +10,35 @@ import java.io.File;
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void explore(File rootDirectory, IFileVisitor visitor) {
 
+        // no file exception here
+        File[] files = rootDirectory.listFiles();
+
+
+        try {
+            //first we say hi to all files in the root directory
+            for (File file : files) {
+                if (file.isFile()) {
+                    visitor.visit(file);
+                }
+            }
+
+            // then we recurse on the subfolders
+            for (File file : files) {
+                if (!file.isFile()) {
+                    visitor.visit(file);
+                    explore(file, visitor);
+                }
+            }
+        } catch (NullPointerException npe){
+            // empty folder
+        }
+    }
 }
