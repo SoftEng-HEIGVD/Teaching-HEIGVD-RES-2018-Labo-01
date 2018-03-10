ffd9ebb6e53bf0b881b479930022e286b70d8c6e
SamuelMayor <samuelmayorweb@gmail.com>
2018-03-10 11:24:55 +0100

    Implementation of upper case filter

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -9,24 +9,33 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        String newString = "";
+        for (int i = off; i < len+off; i++) {
+            newString += str.charAt(i);
+        }
+        out.write(newString.toUpperCase());
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        String string = "";
+        for (int i = off; i < len+off; i++) {
+            string += cbuf[i];
+        }
+        out.write(string.toUpperCase());
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        String charS = "" + (char)c;
+        out.write(charS.toUpperCase());
+    }
 
 }
