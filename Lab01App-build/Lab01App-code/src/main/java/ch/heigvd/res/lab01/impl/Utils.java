afc76e9ae8c3ab171d4bb8a29241e014c9b6f3c2
Daniel Gonzalez Lopez <idaniel.gonzalezlopezi@gmail.com>
2018-03-10 20:43:43 +0100

    Implemented getNextLine() in Utils class. 4 UtilsTest tests passed.

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -7,20 +7,49 @@ import java.util.logging.Logger;
  * @author Olivier Liechti
  */
 public class Utils {
-
+  
   private static final Logger LOG = Logger.getLogger(Utils.class.getName());
-
+  
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
    * the next line in the string passed in arguments. 
-   * 
+   *
    * @param lines a string that may contain 0, 1 or more lines
    * @return an array with 2 elements; the first element is the next line with
    * the line separator, the second element is the remaining text. If the argument does not
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String separator;
+    
+    String[] splitted;
+    String[] result = {"", ""};
+    
+    // With this conditional block, we look for a possible separator.
+    // If there isn't one, we return the array {"", lines}.
+    if (lines.contains("\r\n")) {
+      separator = "\r\n";
+    } else if (lines.contains("\r")) {
+      separator = "\r";
+    } else if (lines.contains("\n")) {
+      separator = "\n";
+    } else {
+      result[1] = lines;
+      
+      return result;
+    }
+    
+    // If there was a separator, we split the String with it.
+    splitted = lines.split(separator);
+    
+    // We put the first result in the first place.
+    result[0] = splitted[0] + separator;
+    
+    // And for all other splitted Strings, we concatenate them into the second result.
+    for(int i = 1; i < splitted.length; ++i) {
+      result[1] += splitted[i] + separator;
+    }
+    
+    return result; 
   }
-
 }
