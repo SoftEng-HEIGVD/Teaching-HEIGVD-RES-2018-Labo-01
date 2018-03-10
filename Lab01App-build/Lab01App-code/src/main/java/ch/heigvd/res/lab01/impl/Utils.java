3fe87182638f73a95d5672c1f4beab45cbbe6b67
Guillaume.Blanco <guillaume.blanco@heig-vd.ch>
2018-03-10 10:04:32 +0100

    Utils

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -20,7 +20,19 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String[] arrayOfLines = {"",""};
+    if(!lines.isEmpty()){
+       if(lines.indexOf('\n') >= 0) {
+         arrayOfLines[0] = lines.substring(0, lines.indexOf('\n') + 1);
+         arrayOfLines[1] = lines.substring(lines.indexOf('\n') + 1);
+       }else if(lines.indexOf('\r') >= 0){
+         arrayOfLines[0] = lines.substring(0, lines.indexOf('\r') + 1);
+         arrayOfLines[1] = lines.substring(lines.indexOf('\r') + 1);
+       }else {
+         arrayOfLines[1] = lines;
+       }
+    }
+    return arrayOfLines;
   }
 
 }
