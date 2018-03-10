5e7d343f322182c57e763a1c346897b967e5bb9a
Labinot Rashiti <labinot.rashiti@heig-vd.ch>
2018-03-10 17:37:53 +0100

    UPDATE, FileNumberingFilterWriter: Implementing methods write

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -14,28 +14,70 @@ import java.util.logging.Logger;
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
  * @author Olivier Liechti
+ * @author Labinot Rashiti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
-
+  private int nbLines = 0;
+  private char lastChar;
+  private boolean beginLine = true;
+  
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      char[] arrayStr = str.toCharArray();
+      write(arrayStr, off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      for (int i = off; i < off + len; ++i) {
+         write(cbuf[i]);
+      }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+     char input = (char) c;
+    
+     // Check if it's the first line
+     if (beginLine) {
+        out.write(++nbLines + "\t"); // Tag the beginning of the line
+        beginLine = false;
+     }
+     
+     // Check last character is a line separator
+     if (lastChar == '\r') {
+        ++nbLines;
+        
+        // Check if there is again a line separator
+        if (input == '\n') {
+           out.write("\n" + nbLines + "\t");
+           lastChar = '\n';
+                    
+        } else if (input != '\n') { // Check if there is no line separator
+           out.write(nbLines + "\t" + input);
+           lastChar = input;
+        }
+       
+       // Check if new line separator
+     } else if (input == '\n') {
+        ++nbLines;
+        out.write("\n" + nbLines + "\t");
+        lastChar = input;
+        
+     } else {          
+        // Normal Input
+        out.write(input);
+        lastChar = input;
+     }
+
+
+      // throw new UnsupportedOperationException("The student has not implemented this method yet.");
   }
 
 }
