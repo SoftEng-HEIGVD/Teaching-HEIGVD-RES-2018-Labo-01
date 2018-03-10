1cf3e31bb2087ec945180e2936066cc119c1a2fe
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-03-10 16:14:44 +0100

    DFSFileExplorerTest: 2/2, 'explore' function prototype corrected

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -2,6 +2,7 @@ package ch.heigvd.res.lab01.impl.explorers;
 
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
+
 import java.io.File;
 
 /**
@@ -9,14 +10,29 @@ import java.io.File;
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void explore(File rootDirectory, IFileVisitor visitor) {
+        // visits anyway the rootDirectory
+        visitor.visit(rootDirectory);
 
+        // end condition
+        if(!rootDirectory.isDirectory())
+            return;
+
+        for(File file : rootDirectory.listFiles()) {
+
+            //visits subdirectories
+            if(file.isDirectory())
+                explore(file, visitor);
+            
+            //visits file
+            else
+                visitor.visit(file);
+        }
+    }
 }
