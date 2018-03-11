dd25f197607472e1ce41f931b83c6685fec01580
Adam <adam.zouari@heig-vd.ch>
2018-03-11 16:08:27 +0100

    UtilsTest success

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -8,19 +8,43 @@ import java.util.logging.Logger;
  */
 public class Utils {
 
-  private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+    private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+    private static final String WIN_SEPARATOR = "\r\n",
+            MACOS9_SEPARATOR = "\r",
+            UNIX_SEPARATOR = "\n";
 
-  /**
-   * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
-   * @param lines a string that may contain 0, 1 or more lines
-   * @return an array with 2 elements; the first element is the next line with
-   * the line separator, the second element is the remaining text. If the argument does not
-   * contain any line separator, then the first element is an empty string.
-   */
-  public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    /**
+     * This method looks for the next new line separators (\r, \n, \r\n) to
+     * extract the next line in the string passed in arguments.
+     *
+     * @param lines a string that may contain 0, 1 or more lines
+     * @return an array with 2 elements; the first element is the next line with
+     * the line separator, the second element is the remaining text. If the
+     * argument does not contain any line separator, then the first element is
+     * an empty string.
+     */
+    public static String[] getNextLine(String lines) {
+
+        String[] nextLine;
+
+        if (!lines.contains(WIN_SEPARATOR) && !lines.contains(MACOS9_SEPARATOR) && !lines.contains(UNIX_SEPARATOR)) {
+            return new String[]{"", lines};
+
+        } else if (lines.contains(WIN_SEPARATOR)) {
+            nextLine = lines.split(WIN_SEPARATOR, 2);
+            nextLine[0] = nextLine[0] + WIN_SEPARATOR;
+
+        } else if (lines.contains(MACOS9_SEPARATOR)) {
+            nextLine = lines.split(MACOS9_SEPARATOR, 2);
+            nextLine[0] = nextLine[0] + MACOS9_SEPARATOR;
+
+        } else {
+            nextLine = lines.split(UNIX_SEPARATOR, 2);
+            nextLine[0] = nextLine[0] + UNIX_SEPARATOR;
+
+        }
+
+        return nextLine;
+    }
 
 }
