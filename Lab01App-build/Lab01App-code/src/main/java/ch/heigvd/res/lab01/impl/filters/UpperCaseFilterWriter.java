dbb17704675f527b8b3825fe4961e569eb5aa429
Olivier Nicole <olivier.nicolesoitnicoulaz@heig-vd.ch>
2018-03-11 15:00:01 +0100

    UpperCaseFilterWriter.java updated and tested OK

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -7,6 +7,7 @@ import java.io.Writer;
 /**
  *
  * @author Olivier Liechti
+ * @author Olivier Nicole (Student)
  */
 public class UpperCaseFilterWriter extends FilterWriter {
   
@@ -16,17 +17,35 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+      /*
+       * Updated by Olivier Nicole
+       * Use toUpperCase from the class String
+       */
+      super.write(str.toUpperCase(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+      /*
+       * Updated by Olivier Nicole
+       * Create a new String with cbuf to call write(String str...)
+       */
+      write(new String(cbuf), off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+      //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      // Use the Character class tu use the static method toUpperCase :
+      //  - https://docs.oracle.com/javase/7/docs/api/java/lang/Character.html
 
+      /*
+       * Updated by Olivier Nicole
+       * Use the Character class tu use the static method toUpperCase :
+       * - https://docs.oracle.com/javase/7/docs/api/java/lang/Character.html
+       */
+      super.write(Character.toUpperCase(c));
+  }
 }
