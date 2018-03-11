0c0c6360f17bb81be1bf5eedd8941c218ebbeba5
Benoît <ben17_1@hotmail.com>
2018-03-11 13:59:41 +0100

    Tous les tests passent!

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -1,7 +1,5 @@
 package ch.heigvd.res.lab01.impl.filters;
 
-import ch.heigvd.res.lab01.impl.Utils;
-
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -31,57 +29,36 @@ public class FileNumberingFilterWriter extends FilterWriter {
 	
 	@Override
 	public void write(String str, int off, int len) throws IOException {
-		str = str.substring(off, off + len);
-		write(str);
+		write(str.toCharArray(), off, len);
 	}
 	
 	@Override
 	public void write(char[] cbuf, int off, int len) throws IOException {
-		if (off + len <= cbuf.length) {
-			String str = new String(cbuf);
-			str = str.substring(off, off + len);
-			write(str);
-			
+		for (int i = off; i < len + off && i < cbuf.length; i++) {
+			write(cbuf[i]);
 		}
 	}
 	
 	@Override
 	public void write(int c) throws IOException {
-		if (c == '\r') {
-			previousCharWasOSX = true;
-		} else if (previousCharWasOSX && c == '\n') {
-			this.out.write(WINDOWS + lineNumber++ + '\t');
-			previousCharWasOSX = false;
-		} else {
-			write("" + (char) c);
-		}
-	}
-	
-	@Override
-	public void write(String str) throws IOException {
-		String[] tab = Utils.getNextLine(str);
-		
-		if (isFirstChar) {
+		if (isFirstChar) { // It's the first caracter we write -> first write the lineNumber with the '\t', then write the caracter.
 			isFirstChar = false;
-			this.out.write(lineNumber++ + "\t");
+			this.out.write("" + lineNumber++ + "\t");
 		}
 		
-		if (tab[0].length() > 0) {
-			String separator = tab[0].substring(tab[0].length() - 1);
-			if (tab[0].length() > 2 && tab[0].substring(tab[0].length() - 2).equals("\r")) {
-				this.out.write(lineNumber++ + "\t" + tab[0]);
-			} else if (separator.equals(OSX)) {
-				this.out.write(tab[0] + lineNumber++ + "\t");
-			} else if (separator.equals(LINUX)) {
-				this.out.write(tab[0] + lineNumber++ + "\t");
+		if (previousCharWasOSX && c == LINUX.charAt(0)) { // We detect the WINDOWS separator sequence -> we are on Windows -> we write the WINDOWS separator then the lineNumber with the '\t'.
+			this.out.write(WINDOWS + lineNumber++ + "\t");
+			previousCharWasOSX = false;
+		} else if (c == OSX.charAt(0)) { // we detect OSX separator -> we have to wait for the next one to see if we are on Windows or MacOSX.
+			previousCharWasOSX = true;
+		} else if (c == LINUX.charAt(0)) { // we detect LINUX separator -> we are on Linux -> we write LINUX separator then the lineNumber with the '\t'.
+			this.out.write(LINUX+ lineNumber++ + "\t");
+		} else { // c is not OSX neither LINUX separator -> no need to write lineNumber except if previous char war OSX separator.
+			if (previousCharWasOSX) { // previous char was OSX separator and actual one is not LINUX separator -> It's not the WINDOWS separator sequence -> we are on MacOSX.
+				previousCharWasOSX = false;
+				this.out.write(OSX + lineNumber++ + "\t"); // we write the previous OSX separator first. Then we write the lineNumber with the '\t'.
 			}
-		}
-		if (tab[1].length() > 0) {
-			if (tab[0].length() == 0) {
-				this.out.write(tab[1]);
-			} else {
-				write(tab[1]);
-			}
+			this.out.write(c);
 		}
 	}
 }
