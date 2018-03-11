9f1413d9337dc6036d2cc983bebe23af05cb6bc1
Benoît <ben17_1@hotmail.com>
2018-03-11 13:26:25 +0100

    Tests classe FileNumberingFileWriter ok

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.lab01.impl.filters;
 
+import ch.heigvd.res.lab01.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -14,8 +16,14 @@ import java.util.logging.Logger;
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
-	
 	private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+	private static String LINUX = "\n";
+	private static String WINDOWS = "\r\n";
+	private static String OSX = "\r";
+	
+	private boolean isFirstChar = true;
+	private int lineNumber = 1;
+	private boolean previousCharWasOSX = false;
 	
 	public FileNumberingFilterWriter(Writer out) {
 		super(out);
@@ -23,17 +31,57 @@ public class FileNumberingFilterWriter extends FilterWriter {
 	
 	@Override
 	public void write(String str, int off, int len) throws IOException {
-		//throw new UnsupportedOperationException("The student has not implemented this method yet.");
+		str = str.substring(off, off + len);
+		write(str);
 	}
 	
 	@Override
 	public void write(char[] cbuf, int off, int len) throws IOException {
-		//throw new UnsupportedOperationException("The student has not implemented this method yet.");
+		if (off + len <= cbuf.length) {
+			String str = new String(cbuf);
+			str = str.substring(off, off + len);
+			write(str);
+			
+		}
 	}
 	
 	@Override
 	public void write(int c) throws IOException {
-		//throw new UnsupportedOperationException("The student has not implemented this method yet.");
+		if (c == '\r') {
+			previousCharWasOSX = true;
+		} else if (previousCharWasOSX && c == '\n') {
+			this.out.write(WINDOWS + lineNumber++ + '\t');
+			previousCharWasOSX = false;
+		} else {
+			write("" + (char) c);
+		}
 	}
 	
+	@Override
+	public void write(String str) throws IOException {
+		String[] tab = Utils.getNextLine(str);
+		
+		if (isFirstChar) {
+			isFirstChar = false;
+			this.out.write(lineNumber++ + "\t");
+		}
+		
+		if (tab[0].length() > 0) {
+			String separator = tab[0].substring(tab[0].length() - 1);
+			if (tab[0].length() > 2 && tab[0].substring(tab[0].length() - 2).equals("\r")) {
+				this.out.write(lineNumber++ + "\t" + tab[0]);
+			} else if (separator.equals(OSX)) {
+				this.out.write(tab[0] + lineNumber++ + "\t");
+			} else if (separator.equals(LINUX)) {
+				this.out.write(tab[0] + lineNumber++ + "\t");
+			}
+		}
+		if (tab[1].length() > 0) {
+			if (tab[0].length() == 0) {
+				this.out.write(tab[1]);
+			} else {
+				write(tab[1]);
+			}
+		}
+	}
 }
