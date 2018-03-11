e0308b2628bebd5a8b7369ae54d6487210ca9798
GZeed <34300894+GZeed@users.noreply.github.com>
2018-03-11 16:26:40 +0100

    all test passed

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -1,5 +1,6 @@
 package ch.heigvd.res.lab01.impl;
 
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 /**
@@ -8,19 +9,32 @@ import java.util.logging.Logger;
  */
 public class Utils {
 
-  private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+    private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
-  /**
-   * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
-   * @param lines a string that may contain 0, 1 or more lines
-   * @return an array with 2 elements; the first element is the next line with
-   * the line separator, the second element is the remaining text. If the argument does not
-   * contain any line separator, then the first element is an empty string.
-   */
-  public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    /**
+     * This method looks for the next new line separators (\r, \n, \r\n) to
+     * extract the next line in the string passed in arguments.
+     *
+     * @param lines a string that may contain 0, 1 or more lines
+     * @return an array with 2 elements; the first element is the next line with
+     * the line separator, the second element is the remaining text. If the
+     * argument does not contain any line separator, then the first element is
+     * an empty string.
+     */
+    public static String[] getNextLine(String lines) {
+
+        String[] linesSplited;
+        if (lines.contains("\r\n")) {
+            linesSplited = lines.split("(?<=\r\n)", 2);//|(?<=\r)|(?<=\n)
+        } else if (lines.contains("\r") || lines.contains("\n")) {
+            linesSplited = lines.split("(?<=\\r)|(?<=\\n)", 2);//|(?<=\r)|(?<=\n)
+        } else {
+            linesSplited = new String[]{"", lines};
+        }
+        if (linesSplited.length == 1) {
+            linesSplited = new String[]{linesSplited[0], ""};
+        }
+        return linesSplited;
+    }
 
 }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -1,7 +1,11 @@
 package ch.heigvd.res.lab01.impl.filters;
 
+import ch.heigvd.res.lab01.impl.Utils;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FilterWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.Writer;
 import java.util.logging.Logger;
 
@@ -17,25 +21,63 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private int counter = 1;
+    private char last;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
+    }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+        this.write(str.toCharArray(), off, len);
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        for (int i = off; i <= off + len - 1; i++) {
+            this.write(cbuf[i]);
+        }
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        /*
+        if (car == '\n' && last == '\r') {//windows is \r\n
+            super.write(car);
+            startLine();
+        } else if (car == '\r') {//mac is \r
+            super.write(car);
+            startLine();
+        } else if (car == '\n') { //linux is \n
+            super.write(car);
+            startLine();
+            
+        } else { //other char
+            super.write(car);
+        }
+
+        last = car;*/
+        char car = (char) c;
+        if (counter == 1) {
+            startLine();
+        }
+        if (car != '\n' && last == '\r') {
+            startLine();
+        }
+        super.write(car);
+        if (car == '\n') {
+            startLine();
+        }
+        last = car;
+    }
+
+    private void startLine() throws IOException {
+        for (char car : (counter + "\t").toCharArray()) {
+            super.write((int) car);
+        }
+        counter++;
+    }
 
 }
