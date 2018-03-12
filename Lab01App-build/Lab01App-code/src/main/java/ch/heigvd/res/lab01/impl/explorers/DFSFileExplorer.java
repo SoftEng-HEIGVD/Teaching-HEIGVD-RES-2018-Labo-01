d281fff754e1f704bf8290cfd92da54d68ba1665
Schnoudli <leo.cortes@heig-vd.ch>
2018-03-12 14:07:47 +0100

    Résolution labo

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -1,22 +1,41 @@
 package ch.heigvd.res.lab01.impl.explorers;
 
+import ch.heigvd.res.lab01.impl.transformers.CompleteFileTransformer;
+import ch.heigvd.res.lab01.impl.transformers.FileTransformer;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
  * exploration of the file system and invokes the visitor for every encountered
- * node (file and directory). When the explorer reaches a directory, it visits all
- * files in the directory and then moves into the subdirectories.
- * 
- * @author Olivier Liechti
+ * node (file and directory). When the explorer reaches a directory, it visits
+ * all files in the directory and then moves into the subdirectories.
+ *
+ * @author Olivier Liechti Modified by : Léo Cortès
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void explore(File rootDirectory, IFileVisitor vistor) {
+        // Exlpore the root and list every file inside
+        vistor.visit(rootDirectory);
+        File[] files = rootDirectory.listFiles();
+
+        if (files != null) {
+            Arrays.sort(files);
+        }
+
+        // files must be non null
+        if (files != null) {
+            // Going through each file
+            for (File file : files) {
+                // If the file is a directory, we can explore it
+                //if(file.isDirectory())
+                explore(file, vistor);
+            }
+        }
+    }
 
 }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -6,27 +6,36 @@ import java.io.Writer;
 
 /**
  *
- * @author Olivier Liechti
+ * @author Olivier Liechti Modified by : Léo Cortès
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        // Transfroming the substring in uppercase
+        String majStr = str.substring(off, off + len).toUpperCase();
+        out.write(majStr);
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        // Transforming a sub-tab string uppercase
+        char[] majTab = new char[len];
+        for (int i = off; i < off + len; ++i) {
+            majTab[i - off] += Character.toUpperCase(cbuf[i]);
+        }
+        out.write(majTab);
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        // Transforming a single char uppercase
+        String majC = "" + Character.toUpperCase((char) c);
+        out.write(majC);
+    }
 
 }
