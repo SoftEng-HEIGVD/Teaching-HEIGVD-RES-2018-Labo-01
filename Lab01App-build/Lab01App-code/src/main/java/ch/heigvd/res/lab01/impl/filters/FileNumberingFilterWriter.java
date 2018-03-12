d897556b5447d0e87315d9e2fc18775e92dea5e4
simon <simon.jobin@heig-vd.ch>
2018-03-12 21:27:21 +0100

    Premier commit

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.logging.Logger;
 
+import ch.heigvd.res.lab01.impl.Utils;
+
 /**
  * This class transforms the streams of character sent to the decorated writer.
  * When filter encounters a line separator, it sends it to the decorated writer.
@@ -17,25 +19,91 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    private int numberOfLines = 1;
+    private boolean newLine = true;
+    
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
+    }
+/*
+    @Override
+    public void write(String str, int off, int len) throws IOException {
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+        //Appel a la methode getNextLine() de Utils
+        String lines[] = Utils.getNextLine(str.substring(off, off + len));
+        String nextLine = lines[0];
+        String remainingLines = lines[1];
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+     
+
+        String strTmp = "\t" + nextLine;
+        
+        while(!nextLine.isEmpty()){
+
+            lines = Utils.getNextLine(remainingLines);
+            nextLine = lines[0];
+            remainingLines = lines[1];
+            strTmp += ++numberOfLines + "\t" + nextLine;
+
+        }
+
+
+        strTmp += remainingLines;
+
+
+        super.write(strTmp);
+    }
+*/
+    public void write(String str, int off, int len) throws IOException {
+
+        //Appel a la methode getNextLine de Utils
+        String[] lines = Utils.getNextLine(str.substring(off, off + len));
+        String nextLine = lines[0];
+        String remainingLines = lines[1];
+
+
+        //chaine a afficher a la fin
+        String textToWrite = (newLine ? numberOfLines + "\t" : "") + nextLine;
+
+        //verification de l'existence d'une nouvelle ligne
+        newLine = !nextLine.isEmpty();
+
+        while(!nextLine.isEmpty()){
+
+            //recuperation de la ligne suivante
+            lines = Utils.getNextLine(remainingLines);
+            nextLine = lines[0];
+            remainingLines = lines[1];
+
+            //ajout de la ligne
+            textToWrite += ++numberOfLines + "\t" + nextLine;
+            
+            //newLine est vrai tant que le texte restant (lines[1] n'est pas vide)
+            newLine = !remainingLines.isEmpty();
+        }
+
+        //Enfin, ajout de la derniere ligne restante
+        textToWrite += remainingLines;
+
+        super.out.write(textToWrite);
+    }
+    
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        if(off >= 0 && off + len < cbuf.length) {
+            write(String.valueOf(cbuf), off, len);
+        }
+        
+        return;
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        //Utilisation de la methode getNextLine de Utils
+        super.write(c);
+    }
 
 }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -9,24 +9,36 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        super.write(str.toUpperCase(), off, len);
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        
+        //verification que off et len soit valides
+        if(off >= 0 && off + len <= cbuf.length) {
+            String str = new String(cbuf);
+            //conversion en majuscule
+            this.write(str, off, len);
+        }
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        //conversion des caracteres ascii des lettres minuscules 
+        //en lajuscules
+        if(c >= 97 && c <= 122) {
+            //majuscules entre 65 et 90
+            c -= 32;
+        }
+        super.write(c);
+    }
 
 }
