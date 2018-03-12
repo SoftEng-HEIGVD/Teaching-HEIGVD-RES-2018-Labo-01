bb64779dbcd2218097f52c074a4fd4e9f7501158
Adam <adam.zouari@heig-vd.ch>
2018-03-12 18:32:21 +0100

    UtilsTest success + comments

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -10,8 +10,8 @@ public class Utils {
 
     private static final Logger LOG = Logger.getLogger(Utils.class.getName());
     private static final String WIN_SEPARATOR = "\r\n",
-            MACOS9_SEPARATOR = "\r",
-            UNIX_SEPARATOR = "\n";
+                                MACOS9_SEPARATOR = "\r",
+                                UNIX_SEPARATOR = "\n";
 
     /**
      * This method looks for the next new line separators (\r, \n, \r\n) to
@@ -27,23 +27,25 @@ public class Utils {
 
         String[] nextLine;
 
+        // the doesn't contain any line separator
         if (!lines.contains(WIN_SEPARATOR) && !lines.contains(MACOS9_SEPARATOR) && !lines.contains(UNIX_SEPARATOR)) {
             return new String[]{"", lines};
-
-        } else if (lines.contains(WIN_SEPARATOR)) {
+            
+        } // the argument contain WINDOWS line separator
+        else if (lines.contains(WIN_SEPARATOR)) {
             nextLine = lines.split(WIN_SEPARATOR, 2);
             nextLine[0] = nextLine[0] + WIN_SEPARATOR;
 
-        } else if (lines.contains(MACOS9_SEPARATOR)) {
+        } // the argument contain MACOS9 line separator
+        else if (lines.contains(MACOS9_SEPARATOR)) {
             nextLine = lines.split(MACOS9_SEPARATOR, 2);
             nextLine[0] = nextLine[0] + MACOS9_SEPARATOR;
 
-        } else {
+        } // the argument contain UNIX line separator
+        else {
             nextLine = lines.split(UNIX_SEPARATOR, 2);
             nextLine[0] = nextLine[0] + UNIX_SEPARATOR;
-
         }
-
         return nextLine;
     }
 
