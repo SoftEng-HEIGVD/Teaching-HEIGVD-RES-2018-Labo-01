8eff4a4a0880eaf6c16fd3fa2fd40b891e52bb1a
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2018-03-12 14:09:37 +0100

    first commit

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,8 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -16,7 +18,26 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
 
+      //check if the directory is not null
+      if(!rootDirectory.isDirectory()){
+          return;
+      }
+
+      vistor.visit(rootDirectory);
+
+      //give a list of each file(s)/directory(ies) from the current directory
+      File[] children = rootDirectory.listFiles();
+
+      if(children == null){
+          return;
+      }
+
+      Arrays.sort(children);
+
+      //call the function recursively with each child from the current directory
+      for (File child : children) {
+          explore(child, vistor);
+      }
+  }
 }
